2037939d40ee6614368c1de3802b930607cb65c0
Adam Vinsant <agvinsant@fullsail.edu>
2012-07-29 17:42:26 -0400

    My first commit to my master branch for MIU

---
+++ js/json.js
@@ -0,0 +1,27 @@
+var json = {
+    "chore1": {
+        "choretype": ["Chore Type: ", "Inside"],
+        "chorename": ["Name: ", "Clean Kitchen"],
+        "finishby": ["Finish By: ", "12/31/2012"],
+        "urgency":["Is this chore urgent: ", "No"],
+        "difficulty":["Difficulty: ", "50"],
+        "chorenotes":["Chore Notes: ", "Notes about cleaning kitchen."]
+    },
+    "chore2": {
+        "choretype": ["Chore Type: ", "Outside"],
+        "chorename": ["Name: ", "Mow Lawn"],
+        "finishby": ["Finish By: ", "08/14/2012"],
+        "urgency":["Is this chore urgent: ", "Yes"],
+        "difficulty":["Difficulty: ", "50"],
+        "chorenotes":["Chore Notes: ", "Notes about mowing the lawn."]
+    },
+    "chore3": {
+        "choretype": ["Chore Type: ", "Errand"],
+        "chorename": ["Name: ", "Go to store"],
+        "finishby": ["Finish By: ", "10/31/2012"],
+        "urgency":["Is this chore urgent: ", "No"],
+        "difficulty":["Difficulty: ", "50"],
+        "chorenotes":["Chore Notes: ", "Notes about going to the store."]
+    }
+        
+}
