28685e179db25ce4f4ea4d41cfcf48cae9ee61b0
Adam Vinsant <agvinsant@fullsail.edu>
2012-07-29 18:40:33 -0400

    added up to 10 dummy json data objects.

--- js/json.js
+++ js/json.js
@@ -4,7 +4,7 @@ var json = {
         "chorename": ["Name: ", "Clean Kitchen"],
         "finishby": ["Finish By: ", "12/31/2012"],
         "urgency":["Is this chore urgent: ", "No"],
-        "difficulty":["Difficulty: ", "50"],
+        "difficulty":["Difficulty: ", "5"],
         "chorenotes":["Chore Notes: ", "Notes about cleaning kitchen."]
     },
     "chore2": {
@@ -12,7 +12,7 @@ var json = {
         "chorename": ["Name: ", "Mow Lawn"],
         "finishby": ["Finish By: ", "08/14/2012"],
         "urgency":["Is this chore urgent: ", "Yes"],
-        "difficulty":["Difficulty: ", "50"],
+        "difficulty":["Difficulty: ", "5"],
         "chorenotes":["Chore Notes: ", "Notes about mowing the lawn."]
     },
     "chore3": {
@@ -20,7 +20,7 @@ var json = {
         "chorename": ["Name: ", "Go to store"],
         "finishby": ["Finish By: ", "10/31/2012"],
         "urgency":["Is this chore urgent: ", "No"],
-        "difficulty":["Difficulty: ", "50"],
+        "difficulty":["Difficulty: ", "5"],
         "chorenotes":["Chore Notes: ", "Notes about going to the store."]
     },
     "chore4": {
@@ -28,8 +28,55 @@ var json = {
         "chorename": ["Name: ", "Make Bed"],
         "finishby": ["Finish By: ", "08/31/2012"],
         "urgency":["Is this chore urgent: ", "Yes"],
-        "difficulty":["Difficulty: ", "35"],
-        "chorenotes":["Chore Notes: ", "Notes about going to the store."]
+        "difficulty":["Difficulty: ", "3"],
+        "chorenotes":["Chore Notes: ", "Notes about making the bed."]
+    },
+    "chore5": {
+        "choretype": ["Chore Type: ", "Outside"],
+        "chorename": ["Name: ", "rake leaves"],
+        "finishby": ["Finish By: ", "8/29/2012"],
+        "urgency":["Is this chore urgent: ", "No"],
+        "difficulty":["Difficulty: ", "5"],
+        "chorenotes":["Chore Notes: ", "Notes raking leaves."]
+    },
+    "chore6": {
+        "choretype": ["Chore Type: ", "Errand"],
+        "chorename": ["Name: ", "pay bill"],
+        "finishby": ["Finish By: ", "8/31/2012"],
+        "urgency":["Is this chore urgent: ", "No"],
+        "difficulty":["Difficulty: ", "2"],
+        "chorenotes":["Chore Notes: ", "Notes about paying a bill."]
+    },
+    "chore7": {
+        "choretype": ["Chore Type: ", "Inside"],
+        "chorename": ["Name: ", "Sweep floor"],
+        "finishby": ["Finish By: ", "8/31/2012"],
+        "urgency":["Is this chore urgent: ", "No"],
+        "difficulty":["Difficulty: ", "1"],
+        "chorenotes":["Chore Notes: ", "Notes about sweeping the floor."]
+    },
+    "chore8": {
+        "choretype": ["Chore Type: ", "Outside"],
+        "chorename": ["Name: ", "Take out garbage"],
+        "finishby": ["Finish By: ", "8/31/2012"],
+        "urgency":["Is this chore urgent: ", "Yes"],
+        "difficulty":["Difficulty: ", "2"],
+        "chorenotes":["Chore Notes: ", "Notes about taking out garbage."]
+    },
+    "chore9": {
+        "choretype": ["Chore Type: ", "Errand"],
+        "chorename": ["Name: ", "Go shopping"],
+        "finishby": ["Finish By: ", "8/31/2012"],
+        "urgency":["Is this chore urgent: ", "Yes"],
+        "difficulty":["Difficulty: ", "5"],
+        "chorenotes":["Chore Notes: ", "Notes about going shopping."]
+    },
+    "chore10": {
+        "choretype": ["Chore Type: ", "Inside"],
+        "chorename": ["Name: ", "Dust furniture"],
+        "finishby": ["Finish By: ", "8/31/2012"],
+        "urgency":["Is this chore urgent: ", "No"],
+        "difficulty":["Difficulty: ", "5"],
+        "chorenotes":["Chore Notes: ", "Notes about dusting the furniture."]
     }
-        
 }
